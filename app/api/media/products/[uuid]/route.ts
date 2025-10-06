03c8ae1b030d2c2e0f4592cd4f409e45ec82b246
federicodonati07 <realeaquila.929@gmail.com>
2025-10-07 00:06:09 +0200

    stable, working on deploy

--- app/api/media/products/[uuid]/route.ts
+++ app/api/media/products/[uuid]/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(
-  _req: NextRequest,
+  _req: Request,
   { params }: { params: { uuid: string } }
 ) {
   const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
